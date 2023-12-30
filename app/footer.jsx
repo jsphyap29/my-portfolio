215ec6d296c99c86cea978b5e1a5bea2df8a52ae
Joseph Andrei Yap <andreiyap20@gmail.com>
2023-12-30 15:50:36 +0800

    Latest changes

--- app/footer.jsx
+++ app/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     const current_year = new Date().getFullYear();
     return(
         <div id="section_footer ">
-        <div className='bg-teal-100 text-center py-4 pb-8  transition-all dark:text-black'>
+        <div className='bg-teal-100 text-center py-4  transition-all dark:text-black'>
           <p>Copyright © {current_year}, @jsphyaps, All Rights Reserved.</p>
         </div>
       </div>
